a487cb5b2bfc0aa144034e61bfd7f840bb75d9c8
Zbigniew Romanowski <zbigniew.romanowski@billongroup.com>
2018-06-12 19:50:35 +0200

    Enable dependency tracking

--- build-all.sh
+++ build-all.sh
@@ -10,7 +10,13 @@ export CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
 export CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
 export LDFLAGS="-fprofile-arcs"
 
-./configure --disable-remote --disable-java  --disable-simd --disable-encryption || exit 1
+./configure \
+    --disable-remote \
+    --disable-java  \
+    --disable-simd \
+    --disable-encryption \
+    --enable-dependency-tracking || exit 1
+    
 make -j5 || exit 1
 cd unittests && make plugin || exit 1
 cd ..
