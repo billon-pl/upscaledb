8ae89e1d879c3cc305638ae9ddbea7bb57d88ffa
Zbigniew Romanowski <zbigniew.romanowski@billongroup.com>
2018-06-13 09:11:50 +0200

    Add macro EXPECT_TRUE for statuch checking

--- unittests/issue107.cc
+++ unittests/issue107.cc
@@ -6,6 +6,14 @@
 
 #include <iostream>
 #include <ups/upscaledb.hpp>
+#include <cstdlib>
+
+#define EXPECT_TRUE( st, txt ) \
+if( !st ) \
+{ \
+	std::cout << txt << std::endl; \
+	exit(1); \
+}
 
 
 int main()
@@ -33,6 +41,7 @@ int main()
     }
 
     ups_cursor_t* cur;
+    ups_status_t st;
 
     ups_cursor_create(&cur, db, 0, 0);
 
@@ -40,11 +49,11 @@ int main()
     ups_key_t key = {0}; key.data = &key_data; key.size = sizeof(key_data);
     ups_record_t rec = {0};
 
-    ups_cursor_move(cur, &key, &rec, UPS_CURSOR_FIRST);
-    if((UPS_SUCCESS == ups_cursor_move(cur, &key, &rec, UPS_CURSOR_PREVIOUS)))
-    {
-        std::cout << "Cursor moved from FIRST to PREVIOUS without UPS_KEY_NOT_FOUND" << std::endl;
-    }
+    st = ups_cursor_move(cur, &key, &rec, UPS_CURSOR_FIRST);
+    EXPECT_TRUE( st == UPS_SUCCESS, "ups_cursor_move, UPS_CURSOR_FIRST" );
+    
+    st = ups_cursor_move(cur, &key, &rec, UPS_CURSOR_PREVIOUS);
+    EXPECT_TRUE( st != UPS_SUCCESS, "Cursor moved from FIRST to PREVIOUS without UPS_KEY_NOT_FOUND" );
 
     ups_cursor_close(cur);
 
