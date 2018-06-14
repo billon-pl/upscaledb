765212f4f0dcfdcd2a2e29bfef207d17d0be46d0
Zbigniew Romanowski <zbigniew.romanowski@billongroup.com>
2018-06-14 10:21:02 +0200

    Fix bug in macro EXPECT_TRUE

--- unittests/issue105.cc
+++ unittests/issue105.cc
@@ -7,34 +7,39 @@
 #include <iostream>
 #include <ups/upscaledb.hpp>
 #include <cstdlib>
+#include <vector>
 
 #define EXPECT_TRUE( st, txt )     \
-if( !st )                          \
+if( !( st ) )                      \
 {                                  \
     std::cout << txt << std::endl; \
     exit( 1 );                     \
 }
 
 ups_db_t* create_env( ups_env_t** env );
-void fill_db( ups_db_t* db, ups_txn_t* txn );
-void erase_key( ups_db_t* db, ups_txn_t* txn, uint32_t query );
-void find_key( ups_db_t* db, ups_txn_t* txn, uint32_t query );
+void fill_db( ups_db_t* db, ups_txn_t* txn, const std::vector< uint32_t >& data );
+void erase_key( ups_db_t* db, ups_txn_t* txn, const std::vector< uint32_t >& query );
+void find_key( ups_db_t* db, ups_txn_t* txn, const std::vector< uint32_t >& query );
 void close_env( ups_env_t *env, ups_db_t* db );
 ups_txn_t* create_txn( ups_env_t* env );
+
+std::vector< uint32_t > generate_data();
+std::vector< uint32_t > generate_query( const std::vector< uint32_t >& data );
 
 //
 //
 //
 int main()
 {
-    const uint32_t query = 0;
+    const std::vector< uint32_t > data = generate_data();
+    const std::vector< uint32_t > query = generate_query( data );
 
     ups_env_t* env = nullptr;
     ups_db_t* db = create_env( &env );
 
     ups_txn_t* txn = create_txn( env );
 
-    fill_db( db, txn );
+    fill_db( db, txn, data );
     erase_key( db, txn, query );
     find_key( db, txn, query );
     close_env( env, db );
@@ -63,6 +68,7 @@ ups_db_t* create_env( ups_env_t **env )
     ups_parameter_t params[] =
     {
         { UPS_PARAM_KEY_TYPE, UPS_TYPE_UINT32 },
+        //{ UPS_PARAM_RECORD_TYPE, UPS_TYPE_UINT32 },
         { UPS_PARAM_RECORD_SIZE, 0 },
         { 0, }
     };
@@ -80,8 +86,8 @@ ups_db_t* create_env( ups_env_t **env )
 ups_txn_t* create_txn( ups_env_t* env )
 {
     ups_txn_t* txn = nullptr;
-    ups_status_t st = ups_txn_begin( &txn, env, nullptr, nullptr, 0 );
-    EXPECT_TRUE( st == UPS_SUCCESS, "ups_txn_begin" );
+    // ups_status_t st = ups_txn_begin( &txn, env, nullptr, nullptr, 0 );
+    // EXPECT_TRUE( st == UPS_SUCCESS, "ups_txn_begin" );
     return txn;
 }
 
@@ -89,16 +95,17 @@ ups_txn_t* create_txn( ups_env_t* env )
 //
 //
 //
-void fill_db( ups_db_t* db, ups_txn_t* txn )
+void fill_db( ups_db_t* db, ups_txn_t* txn, const std::vector< uint32_t >& data )
 {
-    const uint32_t item_count = 500;
-
-    for( uint32_t n = 0; n < item_count; n++ )
+    for( uint32_t v : data )
     {
-        ups_key_t key = ups_make_key( &n, sizeof( n ) );
+        // std::cout << "to insert: " << v << std::endl;
 
-        ups_record_t record;
-        bzero( &record, sizeof( record ) );
+        ups_key_t key = ups_make_key( &v, sizeof( v ) );
+
+        // Record has the same value as key!
+        // ups_record_t record = ups_make_record( &v, sizeof( v ) );
+        ups_record_t record = ups_make_record( nullptr, 0 );
 
         const ups_status_t st = ups_db_insert( db, txn, &key, &record, 0 );
         EXPECT_TRUE( st == UPS_SUCCESS, "ups_db_insert" );
@@ -113,15 +120,21 @@ void fill_db( ups_db_t* db, ups_txn_t* txn )
 //
 //
 //
-void erase_key( ups_db_t* db, ups_txn_t* txn, uint32_t query )
+void erase_key( ups_db_t* db, ups_txn_t* txn, const std::vector< uint32_t >& query )
 {
-    ups_key_t key = ups_make_key( &query, sizeof( query ) );
 
-    ups_status_t st = ups_db_erase( db, txn, &key, 0 );
-    EXPECT_TRUE( st == UPS_SUCCESS, "ups_db_erase, UPS_SUCCESS" );
+    for( uint32_t q : query )
+    {
+        // std::cout << "to erase: " << q << std::endl;
 
-    st = ups_db_erase( db, txn, &key, 0 );
-    EXPECT_TRUE( st == UPS_KEY_NOT_FOUND, "ups_db_erase, UPS_KEY_NOT_FOUND" );
+        ups_key_t key = ups_make_key( &q, sizeof( q ) );
+
+        ups_status_t st = ups_db_erase( db, txn, &key, 0 );
+        EXPECT_TRUE( st == UPS_SUCCESS, "ups_db_erase, UPS_SUCCESS" );
+
+        st = ups_db_erase( db, txn, &key, 0 );
+        EXPECT_TRUE( st == UPS_KEY_NOT_FOUND, "ups_db_erase, UPS_KEY_NOT_FOUND" );
+    }
 
     if( txn )
     {
@@ -132,20 +145,27 @@ void erase_key( ups_db_t* db, ups_txn_t* txn, uint32_t query )
 //
 //
 //
-void find_key( ups_db_t* db, ups_txn_t* txn, uint32_t query )
+void find_key( ups_db_t* db, ups_txn_t* txn, const std::vector< uint32_t >& query )
 {
-    ups_key_t key = ups_make_key( &query, sizeof( query ) );
+    for( uint32_t q : query )
+    {
+        std::cout << "to find: " << q << std::endl;
 
-    ups_record_t record;
-    bzero( &record, sizeof( record ) );
+        ups_key_t key = ups_make_key( &q, sizeof( q ) );
 
-    ups_status_t st = ups_db_find( db, txn, &key, &record, UPS_FIND_GEQ_MATCH );
-    EXPECT_TRUE( st == UPS_SUCCESS, "ups_db_find" );
+        ups_record_t rec;
+        bzero( &rec, sizeof( rec ) );
 
+        ups_status_t st = ups_db_find( db, txn, &key, &rec, UPS_FIND_GEQ_MATCH );
+        if ( st == UPS_SUCCESS )
+        {
+            //const uint32_t rec_data = *reinterpret_cast< uint32_t* >( rec.data );
+            //EXPECT_TRUE( q != rec_data , "Key not deleted, A" );
 
-    // std::cout << *reinterpret_cast< uint32_t* >( key.data ) << std::endl;
-
-    EXPECT_TRUE( query != *reinterpret_cast< uint32_t* >( key.data ), "Key not deleted" );
+            const uint32_t key_data = *reinterpret_cast< uint32_t* >( key.data );
+            EXPECT_TRUE( q != key_data , "Key not deleted, B" );
+        }
+    }
 }
 
 //
@@ -158,4 +178,41 @@ void close_env( ups_env_t* env, ups_db_t* db )
 
     st = ups_env_close( env, UPS_AUTO_CLEANUP );
     EXPECT_TRUE( st == UPS_SUCCESS, "ups_env_close" );
+}
+
+//
+//
+//
+std::vector< uint32_t > generate_data()
+{
+    const std::size_t item_count = 100;
+
+    std::vector< uint32_t > data;
+    data.reserve( item_count );
+
+    for( std::size_t i = 0; i < item_count; i++ )
+    {
+        data.push_back( i );
+    }
+
+    return data;
+}
+
+//
+//
+//
+std::vector< uint32_t > generate_query( const std::vector< uint32_t >& data )
+{
+    std::vector< uint32_t > query;
+
+    for( std::size_t i = 0; i < data.size(); i++ )
+    {
+        // take every second item
+        if( i % 2 == 0 )
+        {
+            query.push_back( data[ i ] );
+        }
+    }
+    return query;
+
 }
